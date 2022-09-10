acfe89860df769dbc2cff36567a145f93b13a8e5
pengpeng <501858681@qq.com>
2022-09-10 16:31:35 +0800

    feat: 将对redis中的resourcemap的更新添加springTask定时执行

--- mall-admin/src/main/java/online/pengpeng/mall/component/ResourceRoleRulesRunner.java
+++ mall-admin/src/main/java/online/pengpeng/mall/component/ResourceRoleRulesRunner.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import online.pengpeng.mall.service.UmsResourceService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,9 +19,20 @@ import org.springframework.stereotype.Component;
 public class ResourceRoleRulesRunner implements CommandLineRunner {
     private final UmsResourceService resourceService;
 
+
     @Override
     public void run(String... args) throws Exception {
         resourceService.initResourceRolesMap();
         log.info("load RoleRuleMap to redis success");
+    }
+
+    /**
+     * cron表达式：Seconds Minutes Hours DayofMonth Month DayofWeek [Year]
+     * 10秒更新一次
+     * */
+    @Scheduled(cron = "0/10 * * ? * ?")
+    private void update() {
+        resourceService.initResourceRolesMap();
+        log.debug("update resource map");
     }
 }
---
+++ mall-admin/src/main/java/online/pengpeng/mall/config/SpringTaskConfig.java
@@ -0,0 +1,13 @@
+package online.pengpeng.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author pengpeng
+ * 定时任务相关配置
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
